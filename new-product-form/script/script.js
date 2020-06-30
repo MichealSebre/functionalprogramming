b8295fdbc8bf3d6174a720b4df8773ec36cb21e6
MichealSebre <msebre@gmail.com>
2020-06-30 01:19:01 -0500

    functional

---
+++ new-product-form/script/script.js
@@ -0,0 +1,11 @@
+
+function newProductFields() {
+        alert("Product Number: " + document.getElementById("product-number").value +"\n"
+        + "Product Name: " + document.getElementById("name").value+"\n"
+        +"Unit Price: " + document.getElementById("unit-price").value+"\n"
+        + "Quntity in Stock: " + document.getElementById("quntity-in-stock").value+"\n"
+        + "Supplier: " + document.getElementById("supplier").value+"\n"
+        + "Date Supplied: " + document.getElementById("date-supplied").value);
+    
+     return false;
+}
